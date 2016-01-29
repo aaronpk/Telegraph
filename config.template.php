1b4751d6f90cb2a8df09237fa8ab252ce8710008
Aaron Parecki <aaron@parecki.com>
2016-01-29 09:35:59 -0800

    add template config

---
+++ config.template.php
@@ -0,0 +1,17 @@
+<?php
+class Config {
+  public static $base = 'http://telegraph.dev/';
+
+  public static $ssl = false;
+  public static $secretKey = '000000000000000000000000000000000000000000000';
+
+  public static $clientID = 'http://telegraph.dev/';
+  public static $defaultAuthorizationEndpoint = 'https://indieauth.com/auth';
+
+  public static $db = [
+    'host' => '127.0.0.1',
+    'database' => 'telegraph',
+    'username' => 'root',
+    'password' => ''
+  ];
+}
